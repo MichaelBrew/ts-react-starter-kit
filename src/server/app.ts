9d5b8983d5170ed3e28fb5ca9455760095dcaaba
MichaelBrew <michaelb@digit.co>
2020-09-26 16:37:24 -0400

    Fix serving React app from express

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,6 +3,8 @@ import * as express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("dist"));
+
 app.get("/", (req, res) => {
   // TODO: Fix file path to bundle.js from index.html
   res.sendFile("index.html", { root: `${__dirname}/../client` });
